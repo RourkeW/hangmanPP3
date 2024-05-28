d58de13735148a01a53673f8a86a6bc5043a92dc
Rourke Whitcombe <rourke28whit@gmail.com>
2024-05-28 21:28:51 +0000

    feat: after the meeting I was told about a random word api and to try this embedded in my code, I am going to try it for a bit with 6 letter words for now however the words seem so complex

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import random
 import platform
 
-print("Play HangMan")
+print("Play HangMan!")
 
-wordPool = ['front', 'sip', 'day', 'shorts', 'journal', 'alive', 'heel', 'film', 'carry', 'referee', 'burial', 'thinker', 'crown', 'branch', 'pan', 'exempt', 'dough', 'spy', 'dump', 'collect', 'water', 'license', 'closed', 'paper', 'wire', 'lobby', 'rib', 'posture', 'meaning', 'divide']
+#wordPool = ['front', 'sip', 'day', 'shorts', 'journal', 'alive', 'heel', 'film', 'carry', 'referee', 'burial', 'thinker', 'crown', 'branch', 'pan', 'exempt', 'dough', 'spy', 'dump', 'collect', 'water', 'license', 'closed', 'paper', 'wire', 'lobby', 'rib', 'posture', 'meaning', 'divide']
 # neaten this
 # perhaps find a genre for the toopic of these words i.e. gaming, cars...
 
 # Choose the random word from the wordPool
-randomWord = random.choice(wordPool)
+randomWord = "https://random-word-api.herokuapp.com/word?length=6"
 
 # I need to find a way of making an underscore for each letter in the randomly chosen word and seperate them with a space so that the user can easily identify the amount of letters in the randomly chosen word to guess (for loop maybe?)
 
