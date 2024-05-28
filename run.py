aa4ea24d591820c74008fc60663933edf4f9be2f
Rourke Whitcombe <rourke28whit@gmail.com>
2024-05-28 15:34:51 +0000

    feat: I have added in 30 random <8 character long words to be chosen at random using the random function, I could expand this pool but we will see later

--- run.py
+++ run.py
@@ -2,4 +2,9 @@ import random
 
 print("Play HangMan")
 
-wordPool = ['', '', '', '',]
+wordPool = ['front', 'sip', 'day', 'shorts', 'journal', 'alive', 'heel', 'film', 'carry', 'referee', 'burial', 'thinker', 'crown', 'branch', 'pan', 'exempt', 'dough', 'spy', 'dump', 'collect', 'water', 'license', 'closed', 'paper', 'wire', 'lobby', 'rib', 'posture', 'meaning', 'divide']
+# neaten this
+# perhaps find a genre for the toopic of these words i.e. gaming, cars...
+
+# Choose the random word from the wordPool
+randomWord = random.choice(wordPool)
