1aa38b154277215efb25bd7428dff365e61a4752
Rourke Whitcombe <rourke28whit@gmail.com>
2024-05-28 15:44:08 +0000

    feat: I have created what my hangman will look like before any guesses have been made

--- run.py
+++ run.py
@@ -8,3 +8,21 @@ wordPool = ['front', 'sip', 'day', 'shorts', 'journal', 'alive', 'heel', 'film',
 
 # Choose the random word from the wordPool
 randomWord = random.choice(wordPool)
+
+# I need to find a way of making an underscore for each letter in the randomly chosen word and seperate them with a space so that the user can easily identify the amount of letters in the randomly chosen word to guess (for loop maybe?)
+
+# function that will draw the hangman as the user guesses wrong
+def draw_hangman(wrong_count):
+    if(wrong_count == 0):
+        print("\n------")
+        print("|/")
+        print("|")
+        print("|")
+        print("|\___")
+    elif(wrong_count == 1):
+    elif(wrong_count == 2):
+    elif(wrong_count == 3):
+    elif(wrong_count == 4):
+    elif(wrong_count == 5):
+    elif(wrong_count == 6):
+
