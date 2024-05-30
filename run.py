cb539c6ca8ef2407dae869135e3e248e88fff5c8
Rourke Whitcombe <rourke28whit@gmail.com>
2024-05-30 15:41:57 +0000

    feat: completed a working hangman game, made lots of comments, I want to make it so my program does not stop when I dont enter Y/N so it only accepts those since entering in another cahracter totally ends the function

--- run.py
+++ run.py
@@ -1,125 +1,163 @@
 import random
 import platform
 
-print("Play HangMan!")
-
-wordPool = ['front', 'sip', 'day', 'shorts', 'journal', 'alive', 'heel', 'film', 'carry', 'referee', 'burial', 'thinker', 'crown', 'branch', 'pan', 'exempt', 'dough', 'spy', 'dump', 'collect', 'water', 'license', 'closed', 'paper', 'wire', 'lobby', 'rib', 'posture', 'meaning', 'divide']
+word_Pool = ['front', 'sip', 'day', 'shorts', 'journal', 'alive', 'heel', 'film', 'carry', 'referee', 'burial', 'thinker', 'crown', 'branch', 'pan', 'exempt', 'dough', 'spy', 'dump', 'collect', 'water', 'license', 'closed', 'paper', 'wire', 'lobby', 'rib', 'posture', 'meaning', 'divide']
 # neaten this
 # perhaps find a genre for the toopic of these words i.e. gaming, cars...
+#Use an WordAPI?
 
 # Choose the random word from the wordPool
-randomWord = random.choice(wordPool)
-
-for x in randomWord:
-    print("_", end=" ")
-
-# I need to find a way of making an underscore for each letter in the randomly chosen word and seperate them with a space so that the user can easily identify the amount of letters in the randomly chosen word to guess (for loop maybe?)
+def get_word():
+    word = random.choice(word_Pool)
+    return word.upper()
+#I chose to use the .upper() here since I want the answer to be more visible
 
 # function that will draw the hangman as the user guesses wrong
-def draw_hangman(wrong_count):
-    if(wrong_count == 0):
-        print("\n------")
-        print("|/   |")
-        print("|")
-        print("|")
-        print("|")
-        print("|")
-        print("|\___")
-    elif(wrong_count == 1):
-        print("\n------")
-        print("|/   |")
-        print("|    O")
-        print("|")
-        print("|")
-        print("|")
-        print("|\___")
-    elif(wrong_count == 2):
-        print("\n------")
-        print("|/   |")
-        print("|    O")
-        print("|    |")
-        print("|")
-        print("|")
-        print("|\___")
-    elif(wrong_count == 3):
-        print("\n------")
-        print("|/   |")
-        print("|    O")
-        print("|    \|")
-        print("|")
-        print("|")
-        print("|\___")
-    elif(wrong_count == 4):
-        print("\n------")
-        print("|/   |")
-        print("|    O")
-        print("|    \|/")
-        print("|")
-        print("|")
-        print("|\___")
-    elif(wrong_count == 5):
-        print("\n------")
-        print("|/   |")
-        print("|    O")
-        print("|    \|/")
-        print("|    /")
-        print("|")
-        print("|\___")
-    elif(wrong_count == 6):
-        print("\n------")
-        print("|/   |")
-        print("|    O")
-        print("|    \|/")
-        print("|    / \"")
-        print("|")
-        print("|\___")
+def display_hangman(tries):
+    stages = [  # in hangman, there are only 6 guesses usually so this is the end with the head, mid-section, both arms and both legs, message displays here to tell user that the game is over
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / \
+                   -
+                """,
+                # head, mid-section and one leg comes when 5 guesses are wrong
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, mid-section and botha rms come when 4 guesses are wrong
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, mid-section and left arm come when 3 guesses are wrong
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso comes when 2 guesses are wrong
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head of the hanging man comes when 1 guess is wrong
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # game start here
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
 
-# might have to switch which side the man hangs from
 
-def show_word(correct_guesses):
-    counter = 0
-    correctLetters = 0
-    for char in randomWord:
-        if(char in correct_guesses):
-            print(randomWord[counter], end=" ")
-            correct_letters += 1
+def play(word):
+    # display the unguessed letters as underscores I make a string equal to the random word initially this is false of course
+    word_completion = "_" * len(word)
+    guessed = False
+    # this array holds the letters a user has guessed so later I can make it so they cannot guess it again and I can not deduct a attempt count
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    # Guides the user at the start
+    print("Play HangMan!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    # main game loop below
+    while not guessed and tries > 0:
+        # again its .upper() I want to keep this constant
+        guess = input("Please enter a guess or a word: ").upper()
+        # I found out about this .isalpha() to check that a letter is a-z, this was found on W3 schools
+        # first if is for letters
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already tried this letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the hidden word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Well Done,", guess, "is in the hidden word!")
+                guessed_letters.append(guess)
+                # This is how I am able to index correct letters over the underscores
+                word_as_list = list(word_completion)
+                # list comprehension and using enumerate, I did not learn this on code institute however I found it on various other sources and people have used it to count iterations when looping I wanted to try
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                # I will now iterate over ythe indices to replace the letters when guessed correctly
+                for index in indices:
+                    word_as_list[index] = guess
+                #now I am converting it back to a string
+                word_completion = "".join(word_as_list)
+                # it is possible that the guess completes the word but the user didnt type the word completely so I write if there are no underscores the game is done
+                if "_" not in word_completion:
+                    guessed = True
+        # Below is the conditional for if the user just guesses a full word
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already tried this word", guess)
+            elif guess != word:
+                print(guess, "is not the hidden word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
         else:
-            print("_", end=" ")
-            counter += 1
-        return correct_letters
+            print("Character not alowed.")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else: # I saw on a coding video that you can use plus each side, I wanted to try it here
+        print("Game Over! You ran out of attempts. The word was " + word + ".")
 
-#function to discover letters
-
-#function below shows the underscores for the randomWord chosen
-
-def printWord():
-    print("\r")
-    for char in randomWord:
-        print("\u203E", end=" ")# found this online not sure whether to use this special character for the unknown letters
-
-# After a meeting with my tutor I was advised to use a clear screen style to reproduce the game as it is played rather than risk having things stacked ontop of each other
-
-def clear_screen():
-    # remember to call this after each letter is guessed in the gamplay function
-    if platform.system() == "Windows":
-        if platform.release() in {"10", "11"}:
-            subprocess.run("", shell=True)
-            print("/033c", end="")
-        else: 
-            subprocess.run(["cls"])
-    else:
-        print("/033c", end="")
-
-# Counter for the length of the word the player will guess
-word_to_guess_length = len(randomWord)
-# Wrong guess count starting at 0 since the game is starting
-wrong_guess_count = 0
-current_guess_index = 0
-# current letters that have been guessed altogether
-current_letters_guessed = 0
-#letters guessed correctly counter
-correct_letters = 0
-
-
-
-print("Game is over")
+# This is the main function to run the game once and then promt for a play again
+def main():
+    word = get_word()
+    play(word)
+    while input("Try Again? (Y/N) ").upper() == "Y":
+        #This above makes the function run as long as the user is clicking Y
+        word = get_word()
+        play(word)
+# I found in another YouTube video that you can enter this and the script will run on the command line ASK TUTOR HERE
+if __name__ == "__main__":
+    main()
